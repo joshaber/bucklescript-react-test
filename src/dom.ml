16b78871d6d87fee42cc798e98e7166f7ee438d9
joshaber <joshaber@gmail.com>
2016-11-19 15:47:20 -0500

    Element might not be found

--- src/dom.ml
+++ src/dom.ml
@@ -2,7 +2,7 @@ type document
 external document : document = "" [@@bs.val]
 
 type element
-external getElementById : document -> string -> element = "" [@@bs.send]
+external getElementById : document -> string -> element Js.null = "" [@@bs.send]
 
 type event
 external preventDefault : event -> unit = "" [@@bs.send]
--- src/main.ml
+++ src/main.ml
@@ -1,8 +1,10 @@
 let () =
-  let el = React.div [%bs.obj {className = "outer"}] [|
+  let root = React.div [%bs.obj {className = "outer"}] [|
     React.component MyComponent.t [%bs.obj {name = "willus"}] [||]
   ; React.component MyComponent.t [%bs.obj {name = "tony"}] [||]
   ; React.div [%bs.obj {className = "test"}] [||]
   |] in
   let host = Dom.getElementById Dom.document "host" in
-  React.render el host
+  match Js.Null.to_opt host with
+    | None -> Js.log "Uhhhhhh shit's broke"
+    | Some host -> React.render root host
